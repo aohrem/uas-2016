a99b08fbe2ee7c6af3bc54ae89363a6993a2d92e
aohrem <a_ohre01@uni-muenster.de>
2016-05-28 11:53:11 +0200

    Implement navigation functionality for additional pages

--- app/views/nav.js
+++ app/views/nav.js
@@ -1,16 +1,9 @@
+var layerListOpen = false;
+
 $(document).ready(function() {
-    var layerListOpen = false;
     var moreMenuOpen = false;
 
-    $('#menu').click(function () {
-        if (layerListOpen) {
-            $('#layer-list').fadeOut();
-            layerListOpen = false;
-        } else {
-            $('#layer-list').fadeIn();
-            layerListOpen = true;
-        }
-    });
+    menuClick();
 
     $('html').click(function () {
         if (moreMenuOpen) {
@@ -28,7 +21,59 @@ $(document).ready(function() {
         }
     });
 
-    $('#more-menu').click(function (event) {
-        event.stopPropagation();
-    })
+    $('#more-menu-links').click(function () {
+        openPage('links');
+    });
+
+    $('#more-menu-info').click(function () {
+        openPage('info');
+    });
+
+    $('#more-menu-help').click(function () {
+        openPage('help');
+    });
+
+    $('#more-menu-contact').click(function () {
+        openPage('contact');
+    });
 });
+
+function menuClick() {
+    $('#menu').unbind();
+    $('#menu').click(function () {
+        if (layerListOpen) {
+            $('#layer-list').fadeOut();
+            layerListOpen = false;
+        } else {
+            $('#layer-list').fadeIn();
+            layerListOpen = true;
+        }
+    });
+}
+
+function menuBackClick() {
+    $('#menu').unbind();
+    $('#menu').click(function () {
+        openMap();
+    });
+}
+
+function openPage(item) {
+    if (layerListOpen) {
+        $('#layer-list').fadeOut();
+    }
+    $('#map').fadeOut(function () {
+        $('#page').fadeIn();
+    });
+    $('#menu').attr("src", "images/ic_keyboard_arrow_left_white_48dp.png");
+    menuBackClick();
+    $('#page-box').load('pages/' + item + '.html');
+}
+
+function openMap() {
+    $('#page').fadeOut(function () {
+        $('#map').fadeIn();
+    });
+    $('#menu').attr("src", "images/ic_menu_white_48dp.png");
+    menuClick();
+}
