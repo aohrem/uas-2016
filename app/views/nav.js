406e8949071ca57c9e7f3d358862fb1344e4be5d
David Pardo <dajupaco@gmail.com>
2016-06-07 16:46:48 +0200

    2 tasks were completed in this Check-in:
    1- The video from the FTP was loaded in the web page in order to test the video tag html functioning for working on the events like: play, pause, stop.
    
    2- As a test a Json of videos was created and published so that the web application render the videos stored on this server and eventually create the menu list of videos dynamicaly.

--- app/views/nav.js
+++ app/views/nav.js
@@ -4,6 +4,7 @@ var videoPreviewOpen = false;
 $(document).ready(function() {
     var moreMenuOpen = false;
 
+    loadVideosFromFTP();
     menuClick();
 
     $('html').click(function () {
@@ -44,15 +45,15 @@ $(document).ready(function() {
             $('#video-preview').fadeOut();
             $('#layer-item-click-test').removeClass('active');
             $('#layer-list-check-test').attr('src', 'images/icons/ic_check_box_outline_grey_24dp.png');
-            $('#layer-list-type-test').attr('src', 'images/icons/ic_movie_grey_48dp.png');
-            $('#layer-list-video-preview-test').attr('src', 'images/layers/preview/video_layer_1_inactive.png');
+            /*$('#layer-list-type-test').attr('src', 'images/icons/ic_movie_grey_48dp.png');
+            $('#layer-list-video-preview-test').attr('src', 'images/layers/preview/video_layer_1_inactive.png');*/
             videoPreviewOpen = false;
         } else {
             $('#video-preview').fadeIn();
             $('#layer-item-click-test').addClass('active');
             $('#layer-list-check-test').attr('src', 'images/icons/ic_check_box_green_24dp.png');
-            $('#layer-list-type-test').attr('src', 'images/icons/ic_movie_darkgrey_48dp.png');
-            $('#layer-list-video-preview-test').attr('src', 'images/layers/preview/video_layer_1_active.png');
+            /*$('#layer-list-type-test').attr('src', 'images/icons/ic_movie_darkgrey_48dp.png');
+            $('#layer-list-video-preview-test').attr('src', 'images/layers/preview/video_layer_1_active.png');*/
             videoPreviewOpen = true;
         }
     })
@@ -99,4 +100,19 @@ function openMap() {
     });
     $('#menu').attr('src', 'images/icons/ic_menu_white_48dp.png');
     menuClick();
+}
+
+function loadVideosFromFTP(){
+    var videoArray =[];
+    var url = 'https://api.myjson.com/bins/4v70e';
+
+    $.getJSON( url,function(data) {
+        videoArray.push(data);
+
+        $.each(videoArray, function(index, video) {
+            // In this point the
+            $('#menu_list').append('');
+        });
+    });
+
 }
